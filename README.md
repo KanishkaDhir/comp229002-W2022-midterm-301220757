9066d2f86aa1ccfa001d6325c7be5ef186d0f34f
KanishkaDhir <97758881+KanishkaDhir@users.noreply.github.com>
2022-03-02 18:32:19 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# comp229002-W2022-midterm-301220757
+ Kanishka Dhir Mid-Term Comp229 002
---
+++ config/db.js
@@ -0,0 +1,4 @@
+module.exports = {
+    "AtlasDB": "mongodb+srv://dbadmin:KswxiiFcl33QS5kB@cluster01-301220757.lencd.mongodb.net/midTerm"
+}
+
